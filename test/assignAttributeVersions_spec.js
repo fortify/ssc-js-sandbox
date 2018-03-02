e9543982418f6224360ca3d10a0ecd6c8a4d5321
eran argaman <eranargaman@gmail.com>
2018-03-01 16:57:59 -0800

    Bug 64866 - microfocusbrand: public JS Sandbox repo includes copyright issues in several files

--- test/assignAttributeVersions_spec.js
+++ test/assignAttributeVersions_spec.js
@@ -1,5 +1,5 @@
 /**
- * (c) Copyright [2017] EntIT Software LLC
+* (c) Copyright [2017] Micro Focus or one of its affiliates
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
 *  You may obtain a copy of the License at
